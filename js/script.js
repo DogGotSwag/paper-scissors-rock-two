4e6ae16ba9af2847a7a2c3400456be8f855944d6
Didier <115608922+DogGotSwag@users.noreply.github.com>
2026-09-14 13:48:18 -0600

    Make playRound function

--- js/script.js
+++ js/script.js
@@ -14,5 +14,33 @@ function getHumanChoice() {
     return prompt("paper, scissors or rock???");
 }
 
+function playRound(humanChoice, computerChoice) {
+    humanChoice.toLowerCase();
+    computerChoice.toLowerCase();
+
+    if(humanChoice == computerChoice) {
+        console.log("you tie!");
+    }
+    else if(humanChoice == "paper" && computerChoice == "rock"){
+        console.log("You WIN! Paper beats Rock");
+    }
+    else if(humanChoice == "scissors" && computerChoice == "paper"){
+        console.log("You WIN! Scissors beats Paper");
+    }
+    else if(humanChoice == "rock" && computerChoice == "scissors"){
+        console.log("You WIN! Rock beats Scissors");
+    }
+    else {
+        console.log(`You Lose! ${computerChoice[0].toUpperCase()+computerChoice.slice(1).toUpperCase()} beats ${humanChoice[0]+computerChoice.slice(1)}`);
+    }
+}
+
 let humanScore = 0;
 let computerScore = 0;
+
+playRound("scissors", "rock");
+playRound("scissors", "paper");
+playRound("scissors", "scissors");
+
+
+
